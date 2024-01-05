48fe1d816bfa512ae67efe029abd2f733927cc4e
wangsa <118614191+wangsa17@users.noreply.github.com>
2024-01-05 21:00:28 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [/Eveydaay/()],
+  base: "Everyday",
+  plugins: [vue()],
   build: {
     assetsInlineLimit: 0,
   },
